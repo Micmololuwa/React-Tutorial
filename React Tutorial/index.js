00d1f9a7a3af0d9d9e649c8aa0dd56babe523394
Michael Mololuwa Stephen <99362146+Micmololuwa@users.noreply.github.com>
2022-07-30 17:22:36 +0000

    Stilll learning react basics

--- React Tutorial/index.js
+++ React Tutorial/index.js
@@ -1,1 +1,49 @@
-ReactDom.render(<h1>Hello, everyone!</h1>)
+
+import React from "react"
+// ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+// ReactDOM.render(
+
+//     <ul>
+//         <li>This is the first element</li>
+//         <li>This is the second unordered listr element</li>
+//     </ul>,
+//     document.getElementById("root")
+// )
+
+// function Item() {
+//     return (
+//         <div className="list-container">
+//             <p className="list-header">This are the list of items in my bag for everyday work</p>
+//             <ul className="lists>
+//                 <li>Laptop</li>
+//                 <li>Laptop charger</li>
+//                 <li>HDMI Converter</li>
+//                 <li>Wired Mouse</li>
+//                 <li>Earpiece and lip bam</li>
+//             </ul>
+//         </div>
+//     )
+// }
+
+// Create a navbar in JSX:
+//     - Use the semantic `nav` element as the parent wrapper
+//     - Have an h1 element with the brand name of your "website"
+//     - Insert an unordered list for the other nav elements
+//         - Inside the `ul`, have three `li`s for "Pricing",
+//         "About", and "Contact"
+//     - Don't worry about styling yet - it'll just be plain-looking HTML for now
+
+
+const nav = (
+    <nav>
+        <h1> Creative.Aquila</h1>
+        
+        <ul> 
+            <li>Pricing </li>
+            <li> About </li>
+            <li>Contact </li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(nav, document.getElementById("root"))
