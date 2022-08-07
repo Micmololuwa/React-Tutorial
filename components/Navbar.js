7542769575f4797772a1657e6ec82b823f9319de
Michael Mololuwa Stephen <99362146+Micmololuwa@users.noreply.github.com>
2022-08-07 20:08:20 +0000

    New files and some changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,17 @@
 import React from "react"
+import "../style.css"
 
 export default function Navbar() {
-    return <h1>Navbar component</h1>
+    return (
+
+        <nav>
+            <div className="logo"> 
+                <img className="image" src="../images/reactjs-icon.png" />
+                <h1 className="logo-text" >ReactFacts</h1>
+            </div>
+
+            <h2 className="navigation">React Course - Project 1</h2>
+        </nav>
+        
+    )
 }
