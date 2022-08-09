66d34e801d9741e0ba83ae17fc16c1dd6a1b4605
Michael Mololuwa Stephen <99362146+Micmololuwa@users.noreply.github.com>
2022-08-09 20:08:57 +0000

    Created components, images/icons and other files

---
+++ Digital Business Card/components/social.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Social() {
+    return (
+        <div interest-wrapper>
+            <img src="./images-icons/Twitter Icon.png"/>
+            <img src="./images-icons/Facebook Icon.png"/>
+            <img src="./images-icons/Instagram Icon.png"/>
+            <img src="./images-icons/GitHub Icon.png"/>
+        </div>
+    )
+    
+}
