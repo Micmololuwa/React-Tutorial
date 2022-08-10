b958021edeacf5a665a00d5973fab9514b94e58b
Michael Mololuwa Stephen <99362146+Micmololuwa@users.noreply.github.com>
2022-08-10 21:08:52 +0000

    Added little addition to the profile component

--- Digital Business Card/components/Profile.js
+++ Digital Business Card/components/Profile.js
@@ -4,11 +4,13 @@ export default function Profile() {
 
     return(
         <div className="profile-wrapper">
-            <img src="./images-icons/Rectangle 90.png" />
-
-            <h1 className="name">Laura Smith</h1>
-            <h4 className="designation">Frontend Developer</h4>
-            <p className="mail">laurasmith.website</p>
+            <img className = "profile-image"src="./images-icons/Rectangle 90.png" />
+            <div className="names">
+                <h1 className="name">Laura Smith</h1>
+                <h4 className="designation">Frontend Developer</h4>
+                <p className="mail">laurasmith.website</p>
+            </div>
+            
 
         </div>
     )
